06ac172c5184de64a20dc6339604c65471f4e4dc
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2018-09-04 13:49:59 +0200

    Archivo de configuración y acceso sin proxy

--- curl.php
+++ curl.php
@@ -1,6 +1,5 @@
 <?php
 
-$fichero_prueba_1 = '/home/bwp000/php/init.test';
 $init_test = false;
 $http_200 = 0;
 $http_429 = 0;
@@ -8,13 +7,32 @@ $http_502 = 0;
 $http_resto = 0;
 $total = 0;
 $z = 0; // Estado no esperado
-$ip_origen = '10.66.128.119';
+
+// Datos de configuracion por defecto
+$fichero_prueba_1 = 'init.test';
+$ip_origen = '127.0.0.1';
+$url = "http://127.0.0.1/tests/test.php";
+$arr_header = Array ( "mod-qos-vip: TEST1TEST2TEST3TEST4" );
+
+
+if ( is_readable ( 'config.php' ) ) {
+	include 'config.php';
+}
+
+printf ( "***** Iniciando test de mod-qos *****".PHP_EOL );
+printf ( "*** - Archivo de activacion: %s".PHP_EOL, $fichero_prueba_1 );
+printf ( "*** - IP de origen: %s".PHP_EOL, $ip_origen );
+printf ( "*** - URL de consulta: %s".PHP_EOL, $url );
+printf ( "*** - Cabeceras:".PHP_EOL);
+foreach ( $arr_header as $cabecera ) {
+	printf (  "***   - %s".PHP_EOL, $cabecera );
+}
 
 while ( true ){
 	if ( ! file_exists ( $fichero_prueba_1 ) ) {
-		printf ( PHP_EOL.PHP_EOL.'--- Esperando a que exista '.$fichero_prueba_1.PHP_EOL );
+		printf ( PHP_EOL.PHP_EOL.'--- Esperando a que exista %s'.PHP_EOL, $fichero_prueba_1 );
 	} else {
-		printf ( PHP_EOL.PHP_EOL.'+++ Continuamos tests; existe '.$fichero_prueba_1.PHP_EOL );
+		printf ( PHP_EOL.PHP_EOL.'+++ Continuamos tests; existe %s'.PHP_EOL, $fichero_prueba_1 );
 	}
 	while ( ! file_exists ( $fichero_prueba_1 ) ) {
 		//$init_test = false;
@@ -24,13 +42,12 @@ while ( true ){
 	for ( $i = 1; $i <= 1000 ; $i++ ){
 		$ch = curl_init();
 
+		curl_setopt($ch, CURLOPT_PROXY, '');
 		curl_setopt($ch, CURLOPT_URL, "http://10.66.128.119/tests/test.php");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_INTERFACE, $ip_origen);
-/*		curl_setopt($ch, CURLOPT_HTTPHEADER, Array (
-			"mod-qos-vip: %VBEBahXT1%VBEBahXT1"
-		));
- */
+//		curl_setopt($ch, CURLOPT_HTTPHEADER, $arr_header;
+ 
 		$result = curl_exec($ch);
 		$curl_info = curl_getinfo($ch);
 		if (!curl_errno($ch)) {
@@ -49,7 +66,7 @@ while ( true ){
 				break;
 			default:
 				$http_resto++;
-				printf ( $curl_info['http_code'] );
+				//printf ( $curl_info['http_code'] );
 				//printf ( PHP_EOL.curl_error($ch).PHP_EOL );
 			}
 		} else {
---
+++ example_config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Datos de configuracion personalizados
+$fichero_prueba_1 = 'init.test';
+$ip_origen = '127.0.0.1';
+$url = "http://127.0.0.1/tests/test.php";
+$arr_header = Array ( "mod-qos-vip: TEST1TEST2TEST3TEST4" );
+
